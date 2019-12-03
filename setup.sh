de2f600e587f2309a04c43508293caeadf661b5c
Shankar Roy <shankar-roy@users.noreply.github.com>
2019-12-02 22:45:11 -0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -51,4 +51,38 @@ opencv_ver=3.4.8
 wget https://github.com/opencv/opencv/archive/$opencv_ver.zip -O opencv.zip
 wget https://github.com/opencv/opencv_contrib/archive/$opencv_ver.zip -O opencv_contrib.zip
 
+unzip opencv.zip
+unzip opencv_contrib.zip
 
+mv opencv-$opencv_ver opencv
+mv opencv_contrib-$opencv_ver opencv_contrib
+
+# Dependency forcompiling OpenCV
+sudo apt-get install -y build-essential cmake unzip pkg-config
+sudo apt-get install -y libjpeg-dev libpng-dev libtiff-dev
+sudo apt-get install -y libjasper-dev
+# If missing do the following
+sudo add-apt-repository "deb http://security.ubuntu.com/ubuntu xenial-security main"
+sudo apt update
+sudo apt install libjasper1 libjasper-dev
+sudo add-apt-repository "deb http://security.ubuntu.com/ubuntu xenial-security main"
+sudo apt update
+sudo apt install -y libjasper1 libjasper-dev
+
+sudo apt-get install -y libavcodec-dev libavformat-dev libswscale-dev libv4l-dev
+sudo apt-get install -y libxvidcore-dev libx264-dev
+
+sudo apt-get install -y libgtk-3-dev
+sudo apt-get install -y libatlas-base-dev gfortran
+
+cd ~/opencv
+mkdir build
+cd build
+cmake -D CMAKE_BUILD_TYPE=RELEASE \
+    -D CMAKE_INSTALL_PREFIX=/usr/local \
+    -D WITH_CUDA=ON \
+    -D INSTALL_PYTHON_EXAMPLES=ON \
+    -D OPENCV_EXTRA_MODULES_PATH=~/opencv_contrib/modules \
+    -D OPENCV_ENABLE_NONFREE=ON \
+    -D WITH_GSTREAMER=ON \
+    -D BUILD_EXAMPLES=ON ..
