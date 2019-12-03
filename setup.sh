3f0495a5bbc2fb41958845842a4a1e45b44939a7
Shankar Roy <shankar-roy@users.noreply.github.com>
2019-12-02 21:52:34 -0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
 # Add user to password less sudo
-# echo $USER' ALL=(ALL) NOPASSWD:ALL' >> sudo visudo
+echo $USER' ALL=(ALL) NOPASSWD:ALL' | sudo tee -a /etc/sudoers > /dev/null
 
 sudo apt update -y
 sudo apt upgrade -y
 
+# Add swap file
+sudo fallocate -l 8G /mnt/8GB.swap
+sudo mkswap /mnt/8GB.swap
+sudo swapon /mnt/8GB.swap
+echo '/mnt/8GB.swap  none  swap  sw 0  0' | sudo tee -a /etc/fstab > /dev/null
+
 sudo apt install -y git
 
+
+sudo apt install python3    
 #Setup Python3 as the default 
 sudo ln -fs /usr/bin/python3 /usr/bin/python
 sudo apt install -y python3-pip
@@ -28,4 +36,12 @@ read -sp 'github Password: ' passvar
 git config --global user.password $passvar
 git config --global user.email "shankar.roy@gmail.com"
 
-
+# Setup cuda
+# https://developer.nvidia.com/cuda-downloads?target_os=Linux&target_arch=x86_64&target_distro=Ubuntu&target_version=1804&target_type=deblocal
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-ubuntu1804.pin
+sudo mv cuda-ubuntu1804.pin /etc/apt/preferences.d/cuda-repository-pin-600
+wget http://developer.download.nvidia.com/compute/cuda/10.2/Prod/local_installers/cuda-repo-ubuntu1804-10-2-local-10.2.89-440.33.01_1.0-1_amd64.deb
+sudo dpkg -i cuda-repo-ubuntu1804-10-2-local-10.2.89-440.33.01_1.0-1_amd64.deb
+sudo apt-key add /var/cuda-repo-10-2-local-10.2.89-440.33.01/7fa2af80.pub
+sudo apt-get update
+sudo apt-get -y install cuda
